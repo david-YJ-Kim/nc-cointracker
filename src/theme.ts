e74220428fcf8d9a48d280bc97b15c74b74257a4
DavidKim <115195328+david-YJ-Kim@users.noreply.github.com>
2024-02-11 16:20:00 -0500

    #5.1 Home part One: Make styles for coin main page.

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   bgColor: "#2f3640",
   textColor: "#f5f6fax",
-  accentColor: "#44bd32",
+  accentColor: "#8c7ae6",
 };
