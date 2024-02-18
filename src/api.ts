f4701fa74e5e010e964972846ec02f5feed6e1f9
DavidKim <115195328+david-YJ-Kim@users.noreply.github.com>
2024-02-17 19:42:14 -0500

    #5.12 Price Chart

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = `https://api.coinpaprika.com/v1`;
+const BACKUP_BASE_URL = `https://ohlcv-api.nomadcoders.workers.dev?coinId=`;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
@@ -11,6 +12,14 @@ export function fetchCoinInfo(coinId: string) {
 }
 export function fetchCoinTickers(coinId: string) {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7;
+  return fetch(`${BACKUP_BASE_URL}${coinId}`).then((response) =>
     response.json()
   );
 }
