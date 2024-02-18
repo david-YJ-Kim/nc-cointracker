c0ece7ecae097ea77200b805fafe87a1dca768a1
DavidKim <115195328+david-YJ-Kim@users.noreply.github.com>
2024-02-17 20:09:33 -0500

    #5.13 Price Chart part One
    js chart library
    
    https://apexcharts.com/

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,72 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChat from "react-apexcharts";
+
+interface IHistoricalData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
-    fetchCoinHistory(coinId)
+  const { isLoading, data } = useQuery<IHistoricalData[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChat
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            // theme: {
+            //   mode: "dark",
+            // },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            yaxis: { show: false },
+            xaxis: {
+              labels: { show: false },
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 5,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
